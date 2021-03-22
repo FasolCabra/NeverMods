1d7894c598e5613ae9f3162d0427c5d8c8cef0b7
FasolCabra <81159171+FasolCabra@users.noreply.github.com>
2021-03-22 12:04:38 +0100

    Update and rename Bunger2.js to Bugsnax1.js

--- Bugsnax1.js
+++ Bugsnax1.js
@@ -5,6 +5,7 @@ desc:'A simple example mod that adds hot peppers and hot sauce.',
 engineVersion:1,
 manifest:'modManifest.js',
 requires:['Default dataset*'],
+sheets:{'spicySheet':'img/Sheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 {
 	//The idea in this simple example mod is to add a few elements focused around hot sauce, because hot sauce is great and I use that stuff everywhere.
@@ -13,7 +14,7 @@ func:function()
 	new G.Res({
 		name:'hot pepper',
 		desc:'[hot pepper]s are loaded with capsaicin and, depending on who you ask, may produce a pleasant burn when eaten.',
-		icon:[Bunger.png],
+		icon:[0,0,'spicySheet'],
 		turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
 		partOf:'food',
 		category:'food',

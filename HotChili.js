5a7a140890782e0e534d78c72e722b9de9d404bb
FasolCabra <81159171+FasolCabra@users.noreply.github.com>
2021-03-22 11:47:57 +0100

    Update HotChili.js

--- HotChili.js
+++ HotChili.js
@@ -5,7 +5,6 @@ desc:'A simple example mod that adds hot peppers and hot sauce.',
 engineVersion:1,
 manifest:'modManifest.js',
 requires:['Default dataset*'],
-sheets:{'spicySheet':'img/spicyModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
 {
 	//The idea in this simple example mod is to add a few elements focused around hot sauce, because hot sauce is great and I use that stuff everywhere.
